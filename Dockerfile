af1393f3db786a84d3a0376432373947b23aba63
Ka Mok <Ka.Mok1@ibm.com>
2018-12-27 15:38:58 -0500

    Add sample for migration

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM solr:7.5.0
 COPY ./sample-data.xml /opt/mysolrhome
+COPY ./example-scenarios/migration-alias/new-data/new-data.xml /opt/mysolrhome
 CMD docker-entrypoint.sh solr start -c -f
--- create-records.sh
+++ create-records.sh
@@ -1,3 +1,5 @@
+## Create collection headlines_1 with sample data
 docker-compose exec solr bin/solr create_collection -c headlines_1 -d /opt/solr/server/solr/headlines/conf
-docker-compose exec solr bin/post -c headlines_1 /opt/mysolrhome
+docker-compose exec solr bin/post -c headlines_1 /opt/mysolrhome/sample-data.xml
+## Set alias headlines to collection headlines_1
 curl "http://localhost:8983/solr/admin/collections?action=CREATEALIAS&name=headlines&collections=headlines_1"
