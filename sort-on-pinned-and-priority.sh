05d24fcd258e82f4d6bc23415bf08b8904d8749d
Nate <nate@Strides-MacBook-Pro.local>
2018-12-27 14:30:52 -0500

    Use quotes in curl scripts

--- sort-on-pinned-and-priority.sh
+++ sort-on-pinned-and-priority.sh
@@ -1,1 +1,1 @@
-curl http://localhost:8983/solr/headlines/select?q=*:*&sort=pinned%20asc,%20priority%20asc
+curl "http://localhost:8983/solr/headlines/select?q=*:*&sort=pinned%20asc,%20priority%20asc"
--- sort-on-status.sh
+++ sort-on-status.sh
@@ -1,1 +1,1 @@
-curl http://localhost:8983/solr/headlines/select?q=*:*&sort=status%20asc
+curl "http://localhost:8983/solr/headlines/select?q=*:*&sort=status%20asc"
