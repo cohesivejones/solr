f6e2326b41bf9abe2cdbe1bd4d0cf87b822fd062
Ka Mok <Ka.Mok1@ibm.com>
2018-12-27 14:22:25 -0500

    Add docker config to start Solr in Cloud Mode; add an alias of headlines

--- create-records.sh
+++ create-records.sh
@@ -1,1 +1,3 @@
-docker-compose exec solr bin/post -c headlines /opt/mysolrhome
+docker-compose exec solr bin/solr create_collection -c headlines_1 -d /opt/solr/server/solr/headlines/conf
+docker-compose exec solr bin/post -c headlines_1 /opt/mysolrhome
+curl "http://localhost:8983/solr/admin/collections?action=CREATEALIAS&name=headlines&collections=headlines_1"
